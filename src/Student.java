677e22b58bd0f5c049b83c70beed94ac735af157
Dell <divyangana321@gmail.comj>
2023-10-04 06:41:14 -0700

    First commit

---
+++ src/Student.java
@@ -0,0 +1,63 @@
+
+class Student {
+
+    private String name;
+    private int age;
+    private char grade;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+
+    public char getGrade() {
+        return grade;
+    }
+
+
+    public void setGrade(char newGrade) {
+        grade = newGrade;
+    }
+
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+    }
+}
+
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+
+        student.setName("Alice Johnson");
+        student.setAge(18);
+        student.setGrade('B');
+
+
+        System.out.println("Student Information:");
+        student.displayInfo();
+    }
+}
+
